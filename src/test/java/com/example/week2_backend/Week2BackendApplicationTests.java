a4be0f721c1515051aa2b03e16c684d82de0e101
kjb95 <topmedia44@gmail.com>
2023-01-30 18:03:45 +0900

    feat: 스프링부트 프로젝트 생성

---
+++ src/test/java/com/example/week2_backend/Week2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
